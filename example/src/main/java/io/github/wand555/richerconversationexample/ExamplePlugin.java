672cc9c18cf028c69d692c9ec3c05755e1c619be
wand555 <wand516@gmail.com>
2022-02-15 20:32:39 +0100

    restructure project

---
+++ example/src/main/java/io/github/wand555/richerconversationexample/ExamplePlugin.java
@@ -0,0 +1,41 @@
+package io.github.wand555.richerconversationexample;
+
+import io.github.wand555.richerconversation.RicherConversation;
+import io.github.wand555.richerconversation.RicherConversationFactory;
+import io.github.wand555.richerconversationexample.prompts.BaseComponentPrompt;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.conversations.Conversation;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExamplePlugin extends JavaPlugin implements CommandExecutor {
+
+    @Override
+    public void onEnable() {
+        getCommand("showbasecomponentsupport").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            return false;
+        }
+        Player player = (Player) sender;
+        Conversation conversation = new RicherConversationFactory(this)
+                .withLocalEcho(true)
+                .withModality(true)
+                .withPrefix(context -> ChatColor.of("#E27D60") + "[" + ChatColor.of("#41B3A3") + "ExamplePlugin" + ChatColor.of("#E27D60") + "] ")
+                .withFirstPrompt(new BaseComponentPrompt())
+                .buildConversation(player);
+        conversation.begin();
+        return true;
+    }
+}
---
+++ example/src/main/java/io/github/wand555/richerconversationexample/prompts/BaseComponentPrompt.java
@@ -0,0 +1,41 @@
+package io.github.wand555.richerconversationexample.prompts;
+
+import io.github.wand555.richerconversation.prompts.RicherPrompt;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.conversations.ConversationContext;
+import org.bukkit.conversations.MessagePrompt;
+import org.bukkit.conversations.Prompt;
+
+public class BaseComponentPrompt extends MessagePrompt implements RicherPrompt {
+
+    @Override
+    public BaseComponent getRicherPromptText(ConversationContext context) {
+        return new TextComponent(new ComponentBuilder()
+                .append("I'm a ")
+                .color(ChatColor.of("#C38D9E"))
+                .append("message created from")
+                .underlined(true)
+                .append(" base components")
+                .color(ChatColor.of("#3FEEE6"))
+                .underlined(false)
+                .bold(true)
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(new ComponentBuilder()
+                        .append("Yes, really!")
+                        .color(ChatColor.of("#E8A87C"))
+                        .create())))
+                .append(" in a conversation prompt.")
+                .bold(false)
+                .color(ChatColor.of("#C38D9E"))
+                .create());
+    }
+
+    @Override
+    protected Prompt getNextPrompt(ConversationContext context) {
+        return END_OF_CONVERSATION;
+    }
+}
