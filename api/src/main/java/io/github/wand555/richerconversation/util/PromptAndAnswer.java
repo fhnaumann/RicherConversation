ae71ee7fec4a03c24320bc322cdf1a028ebbf52b
wand555 <wand516@gmail.com>
2022-02-18 19:12:26 +0100

    javadoc for promptandanswer record

--- api/src/main/java/io/github/wand555/richerconversation/util/PromptAndAnswer.java
+++ api/src/main/java/io/github/wand555/richerconversation/util/PromptAndAnswer.java
@@ -2,5 +2,9 @@ package io.github.wand555.richerconversation.util;
 
 import org.bukkit.conversations.Prompt;
 
+/**
+ * Record storing the prompt and the answer given by the user.
+ * This is mainly used to keep track of the history during a conversation.
+ */
 public record PromptAndAnswer(Prompt prompt, String answer) {
 }
